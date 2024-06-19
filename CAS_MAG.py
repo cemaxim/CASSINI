1087d860556733beb2aa6200af597bcf7188a039
cemaxim <cemaxim@alaska.edu>
2024-06-19 10:48:47 -0800

    Add files via upload

--- CAS_MAG.py
+++ CAS_MAG.py
@@ -8,43 +8,31 @@ Created on Thu May 23 09:40:26 2024
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.markers import MarkerStyle
-import matplotlib.dates as mdates
 import numpy as np
 from scipy import signal
 from SPICE_POS import SPICE_POS
 from CAS_ORBITS import ORBIT
 import pycwt as wavelet
-import pywt
-#start_time = 'Feb 1, 2007'
-#end_time = 'Mar 15, 2007'
-#time = [start_time, end_time]
+import os
 
-n=24 #orbit number 0-119
-m=36
-t1 = ORBIT(n)
-t2 = ORBIT(m)
-time = [t1[0], t1[1]]
-
-start = t1[0]
-end = t2[1]
-#dti = pd.date_range(start, end, freq = 'min')
-
-"GET DATA"
-mag_df = pd.read_pickle("/home/cemaxim/pickles/MAG_DF.pkl")
-mag_df = mag_df[time[0] : time[1]]
-#mag_df = mag_df.resample('1T',origin=t1[0]).fillna(method=None)  #fill data for missing datetimes with NaN
-
-bc_df = pd.read_pickle('/home/cemaxim/pickles/BC_ID.pkl')
-df = SPICE_POS(mag_df,time)
-df = pd.merge_asof(df, bc_df, left_index=True, right_index=True, direction='forward')
-df.insert(0,'R',np.sqrt(df.x**2 + df.y**2 + df.z**2))
+def get_data(orbit_i,orbit_f=None): #get data from start and end time of orbit(s)
+    start = ORBIT(orbit_i)[0]
+    if orbit_f is not None:
+       end = ORBIT(orbit_f)[1]
+    else:
+        end = ORBIT(orbit_i)[1]
+    time = [start, end]
+    mag_df = pd.read_pickle("/home/cemaxim/pickles/MAG_DF.pkl")
+    mag_df = mag_df[time[0] : time[1]]
+    bc_df = pd.read_pickle('/home/cemaxim/pickles/BC_ID.pkl')
+    df = SPICE_POS(mag_df,time)
+    df = pd.merge_asof(df, bc_df, left_index=True, right_index=True, direction='forward')
+    df.insert(0,'R',np.sqrt(df.x**2 + df.y**2 + df.z**2))
+    return time, df, bc_df
 
 
-def plot_orbit(id, data):#, start_time, end_time ,n):
-    #t = [start_time, end_time]
-    #c_data = id[t[0] : t[1]]
-    #data = data[t[0] : t[1]]
-    fig, ax = plt.subplots(figsize=(9,9))  
+def plot_orbit(data, id, R_min, n, time):
+    fig, axs = plt.subplots(figsize=(9,9))  
     for i in range(len(id.index)-1):
         t = data.index
         t1 = id.index[i]
@@ -52,6 +40,7 @@ def plot_orbit(id, data):#, start_time, end_time ,n):
         wh = (t>t1) & (t<t2)
         x = data.iloc[wh]['x'].to_numpy()
         y = data.iloc[wh]['y'].to_numpy()
+        
         if id.iloc[i]['ID'] == -1:
             clr = '-r'
             #label = 'solar wind'
@@ -61,24 +50,22 @@ def plot_orbit(id, data):#, start_time, end_time ,n):
         if id.iloc[i]['ID'] == 1:
             clr = '-b'
             #label = 'magnetosphere'
-        #if id.iloc[i]['ID'] == -2:
-            #clr = '-c'
-        ax.plot(x, y, clr)#, 'o', markersize=10, markevery=[0,-1])
-        ax.set_xlabel('x ($R_S$)')
-        ax.set_ylabel('y ($R_S$)')
-        ax.plot(0,0,markersize = 15, color = 'black', markerfacecoloralt='white',marker=MarkerStyle("o", fillstyle='left'))
+        axs.plot(x, y, clr, linestyle=':')
+        axs.set_xlabel('x ($R_S$)')
+        axs.set_ylabel('y ($R_S$)')
+        axs.plot(0,0,markersize = 15, color = 'black', markerfacecoloralt='white',marker=MarkerStyle("o", fillstyle='left'))
         plt.gca().set_aspect('equal')
-        ax.grid(True, linestyle=':')
-        ax.set_title('Cassini Orbit '+str(n))#Position from\n'+str(time[0])+' to '+str(time[1]))
-    
-def B_vs_R(data, R_min, win, start_time, end_time):
-    
+        axs.grid(True, linestyle=':')
+        axs.set_title('Cassini Orbit '+str(n)+'\n['+str(time[0])+' to '+str(time[1])+']')        
+    data = data.loc[(data.R > R_min)]
+    axs.plot(data.x.values,data.y.values,'-b', label='$R \geq$'+str(R_min))
+    fig.tight_layout()
+    plt.legend()
+     
+        
+def B_vs_R(data, R_min, win, start_time, end_time):   
     data = data[start_time : end_time]
-    #data = pd.merge_asof(data, id, left_index=True, right_index=True, direction='forward')
-    data = data.loc[(data.R > R_min) & (data.ID ==1)]
-    data = data.resample('1T',origin=t1[0]).fillna(method=None)
-    #is_nan = data['BX'].isna() 
-    
+    data = data.loc[(data.R > R_min) & (data.ID ==1)] 
     fig, axs = plt.subplots(3,sharey=True)
     fig.set_size_inches((12,8))
     fig.supxlabel('Radial Distance ($R_S$)')
@@ -91,46 +78,44 @@ def B_vs_R(data, R_min, win, start_time, end_time):
     axs[2].plot(data.R.values, data.BPHI.values)
     axs[2].plot(data.R.values, data.BPHI.rolling(win,center=True).mean(),'-r')
     axs[2].set_ylabel('$B_{\phi}$ (nT)')
-"""
-def filter(data, R_lim, ID):
-    data = data.loc[(data.R > R_lim) & (data.ID ==1)]
-    return data
-"""
 
-def MFA(data, win, R_min, start, end): #mean field align
+
+def MFA(data, win, R_min): #mean field align
     data = data.loc[(data.R > R_min) & (data.ID ==1)]
     means = pd.DataFrame({'BX': data.BX.rolling(win,center=True).mean(),
                           'BY': data.BY.rolling(win,center=True).mean(),
                           'BZ': data.BZ.rolling(win,center=True).mean()})
     means = means.dropna()
-    means = means[:][start : end]
     means_mags = np.sqrt(means.BX**2+means.BY**2+means.BZ**2)
     raw = data[['BX', 'BY', 'BZ']]
     raw = raw.reindex_like(means,None)
-    raw = raw[:][start : end]
-    raw_mags = np.sqrt(data.BX**2+data.BY**2+data.BZ**2)
+    raw_mags = np.sqrt(raw.BX**2+raw.BY**2+raw.BZ**2)
     li = []
     for t in range(len(means.index)):
         z_hat = means.iloc[t]/means_mags.iloc[t]
-        R_hat = raw.iloc[t]/raw_mags.iloc[t]
+        z_hat = z_hat.to_numpy()
+        R_hat = np.array([1,0,0])#raw.iloc[t]/raw_mags.iloc[t]
         y_hat = np.cross(z_hat,R_hat)/np.sqrt(np.sum(np.cross(z_hat,R_hat)**2))
         x_hat = np.cross(y_hat,z_hat)
-        bx = np.dot(raw.iloc[t].to_numpy()-means.iloc[t].to_numpy(),x_hat)
-        by = np.dot(raw.iloc[t].to_numpy()-means.iloc[t].to_numpy(),y_hat)
-        bz = np.dot(raw.iloc[t].to_numpy()-means.iloc[t].to_numpy(),z_hat)   
-        #temp = pd.DataFrame({'B_PERP1':[bx], 'B_PERP2':[by], 'B_PAR':[bz]})#, index = means.index[t])
-        #li.append(temp)
+      # Perturbation Components:
+        db = raw.iloc[t].to_numpy()-means.iloc[t].to_numpy()
+        bx = np.dot(db, x_hat) 
+        by = np.dot(db, y_hat)
+        bz = np.dot(db, z_hat)   
         li.append({'B_PERP1':bx, 'B_PERP2':by, 'B_PAR':bz})
     mfa_data = pd.DataFrame(li)
     mfa_data = mfa_data.set_index(means.index)
     data_R = data[['R']][mfa_data.index[0] : mfa_data.index[-1]]
     mfa_data = pd.merge_asof(data_R, mfa_data, left_index=True, right_index=True, direction='forward')
-    
+    return mfa_data#, means, raw, means_mags, raw_mags
 
+def MFA_plot(mfa_data):#, means, raw, means_mags, raw_mags):
+    #means_mags = np.sqrt(means_mags.BX**2+means_mags.BY**2+means_mags.BZ**2)
     fig, axs = plt.subplots(4, sharex=True)
     fig.set_size_inches((12,8))
-    fig.tight_layout()
+   
     fig.supxlabel('TIME')
+    fig.suptitle('Magnetic Field Perturbations (Mean-Field-Aligned)')
     axs[0].plot(mfa_data.index.values, mfa_data.B_PERP1.values)
     axs[0].set_ylabel('B_PERP1 (nT)')
     #axs[0].grid()
@@ -142,15 +127,20 @@ def MFA(data, win, R_min, start, end): #mean field align
     #axs[2].grid()
     axs[3].plot(mfa_data.index.values, mfa_data.R.values)
     axs[3].set_ylabel('Radial Distance ($R_S$)')
-   # axs[3].get_shared_x_axes().join(axs[0], axs[1], axs[2], axs[3])
-    return mfa_data
+    
+    #p_mag = np.sqrt(mfa_data.B_PERP1**2+mfa_data.B_PERP2**2+mfa_data.B_PAR**2)
+    #d = raw_mags - means_mags
+    #axs[4].plot(p_mag)
+    #d = raw - means
+    #d_mag = np.sqrt(d.BX**2+d.BY**2+d.BZ**2)
+    #axs[5].plot(d_mag)
+    fig.tight_layout()
 
 
-def CWT(data, freq):
+def CWT(data, freq='15min'): #Continuous Wavelet Analysis... Downsample data w freq
     data = data.asfreq(freq)
-    #data = data.resample(n, origin = 'start').asfreq()
-    #data = data.iloc[::n] #downsample
-    et = (data.index.to_series() - data.index.min()).dt.total_seconds()
+    data = data.interpolate()
+    et = (data.index.to_series() - data.index.min()).dt.total_seconds() #elapsed time in seconds dataframe 
     t = et.values
     dt = t[1]-t[0]
         
@@ -162,48 +152,75 @@ def CWT(data, freq):
     
     cwtmatr = signal.cwt(data, signal.morlet2, widths, w = omega0)
     #cwtmatr, freqs = wavelet.cwt(data,widths,"cmor2.5-1.0", sampling_period = dt)
-    
+    """
     ha = plt.subplot(111)
     ha.pcolormesh(t,f,abs(cwtmatr[:,:-1])**2, cmap='turbo')
     ha.set_yscale('log')
     ha.set_ylim(f[1],f[-1])
     ha.set_xlabel('Time')
     ha.set_ylabel('Frequency [Hz]')
-    
-    
+    """
     mother = wavelet.Morlet(6)
-    s0 = dt
-    dj = 1/12
-    J = 7/dj
-    #alpha, _, _ = wavelet.ar1(data)  # Lag-1 autocorrelation for red noise
+    alpha, _, _ = wavelet.ar1(data)
     
-    wave, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(data.values, dt, dj, s0, wavelet=mother)
+    wave, scales, freqs, coi, fft, fftfreqs = wavelet.cwt(data.values, dt, wavelet=mother, freqs = f)
     
     power = np.abs(wave)**2
-    fft_power = np.abs(fft)**2
+    #fft_power = np.abs(fft)**2
     period = (1/freqs)/3600 #hours
-    """
+    coi = coi**-1
+  # Remove Cone of Infuluence
+    for i, col in enumerate(power.T):
+            col_num = len(col) - i
+            coi_start_index = min(range(len(freqs)),
+                                  key=lambda i: abs(freqs[i] - coi[col_num]))
+            power[:coi_start_index, col_num] = np.zeros(coi_start_index)        
+   # Significance Test
     signif, fft_theor = wavelet.significance(1.0, dt, scales, 0, alpha,
                                          significance_level=0.95,
                                          wavelet=mother)
     sig95 = np.ones([1, N]) * signif[:, None]
     sig95 = power / sig95
-    """
+    
     plt.figure()
-    ha = plt.subplot(111)
-    ha.pcolormesh(t,period,power, cmap='turbo')
-    #ha.contour(t,period,sig95)
-    ha.set_yscale('log')
-    ha.set_ylim(period.min(),period.max())
-    ha.set_xlabel('Time (s)')
-    #ha.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d-%h'))
-    #for label in ax.get_xticklabels(which='major'):
-     #   label.set(rotation=30, horizontalalignment='right')
-    #ha.set_xticklabels(t,rotation=45)
-    ha.set_ylabel('Period (hr)')
-    ha.fill(np.concatenate([t, t[-1:] + dt, t[-1:] + dt,
+    axs = plt.subplot(111)
+    axs.set_title(str(data.name)+' Wavelet Power Spectrum')
+    axs.pcolormesh(t,period,power, cmap='turbo')
+    #axs.contour(t,period,power) sig95
+    axs.set_yscale('log')
+    axs.set_ylim(period.min(),period.max())
+    axs.set_xlabel('Time (s)')
+    axs.set_ylabel('Period (hr)')
+    axs.plot([t.min(),t.max()],[10,10],'--', color='white',linewidth=1)
+    axs.plot([t.min(),t.max()],[15,15],'--',color='white',linewidth=1)
+    axs.plot([t.min(),t.max()],[30,30],'--',color='white',linewidth=1)
+    
+    axs.fill(np.concatenate([t, t[-1:] + dt, t[-1:] + dt,
                            t[:1] - dt, t[:1] - dt]),
         np.concatenate([coi/3600, [1e-1], period[-1:],
                            period[-1:], [1e-1]]),
-        'k', alpha=0.3, hatch='x')
+        'k', alpha=0.5, hatch='x')
+    plt.tight_layout()
+    #plt.colorbar(ha.pcolormesh(t,period,power, cmap='turbo'))
+
     
+def plots(n, R_min, win, freq):
+    time , data, id = get_data(n)
+    plot_orbit(data, id, R_min, n, time)
+    mfa_data = MFA(data, win, R_min)
+    MFA_plot(mfa_data)
+    CWT(mfa_data.iloc[:,1],'15min')
+
+"""
+def save_plots(data,id,R_min,win,freq='15min'):
+    folder_path = '/home/cemaxim/CAS/ORBIT_'+str(n)
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+    plot_orbit(data, id, R_min)
+    plt.savefig(folder_path+'/ORBIT')
+    mfa = MFA(data, win, R_min)
+    MFA_plot(mfa)
+    plt.savefig(folder_path+'/MFA_PERTURBS')
+    CWT(mfa.B_PERP1, freq)
+    plt.savefig(folder_path+'/WPS')
+"""
